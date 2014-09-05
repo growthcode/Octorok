570d94e2e8c2376c32e274575aadc5d5b3110cfa
Vlad Abacumov <vabacumov@gmail.com>
2014-09-04 22:20:30 -0700

    Removes unnecessary comments. Resizes frog image. Adds fluid movement to frog attached to keyboard arrow events.

--- public/js/application.js
+++ public/js/application.js
@@ -2,14 +2,39 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
   game.load.image('frog', '../assets/frog.png');
-  // game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 var platform;
 function create() {
-  game.add.sprite(0, 0,'frog', 32, 48);
+  scale = game.rnd.realInRange(0.2, 0.2);
+  frog = game.add.sprite(600, 0,'frog');
+  frog.scale.x = scale;
+  frog.scale.y = scale;
+  game.physics.arcade.enable(frog);
+
+  frog.body.collideWorldBounds = true;
 }
 
 function update() {
+  // game.physics.arcade.collide(player, cars);
+  // game.physics.arcade.collide(player, logs);
+
+  cursors = game.input.keyboard.createCursorKeys();
+  frog.body.velocity.x = 0;
+  frog.body.velocity.y = 0;
+
+
+  if (cursors.left.isDown){
+    frog.body.velocity.x -= 50;
+  }
+  if (cursors.right.isDown){
+    frog.body.velocity.x = 150;
+  }
+  if (cursors.up.isDown){
+    frog.body.velocity.y = -150;
+  }
+  if (cursors.down.isDown){
+    frog.body.velocity.y = 150;
+  }
 }
 
