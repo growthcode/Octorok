5e0f40748dfb16ebd33e84e4a4fcfa759d65c352
Vlad Abacumov <vabacumov@gmail.com>
2014-09-04 22:25:55 -0700

    Removes water.height bug. Adds spacing withing create function.

--- public/js/application.js
+++ public/js/application.js
@@ -10,25 +10,26 @@ function preload() {
 var platform;
 function create() {
   frogScale = game.rnd.realInRange(0.1,0.1);
-  frog=game.add.sprite(0, 540,'frog');
+  frog = game.add.sprite(0, 540,'frog');
   game.physics.arcade.enable(frog);
   frog.body.collideWorldBounds = true;
   frog.scale.x = frogScale;
   frog.scale.y = frogScale;
+
   carScale = game.rnd.realInRange(0.15,0.15);
-  car=game.add.sprite(0, 500,'car');
+  car = game.add.sprite(0, 500,'car');
   car.scale.x = carScale;
   car.scale.y = carScale;
+
   waterScale = game.rnd.realInRange(.5,.5);
-  water=game.add.sprite(-170, 65,'water');
+  water = game.add.sprite(-170, 65,'water');
   water.scale.x = waterScale*2.2;
   water.scale.y = waterScale;
+
   logScale = game.rnd.realInRange(0.2,0.2);
-  log=game.add.sprite(0, 200,'log');
+  log = game.add.sprite(0, 200,'log');
   log.scale.x = logScale;
   log.scale.y = logScale;
-  water.style.height="50px"
-
 }
 
 function update() {
