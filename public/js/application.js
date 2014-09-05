a9422e53c333253b678474ca2ceecd62c0263c7b
Xang Thao <xangthao24@yahoo.com>
2014-09-05 15:55:59 -0700

    Adds changes to variable naming.

--- public/js/application.js
+++ public/js/application.js
@@ -1,27 +1,27 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 
-var left = 0;
-var right = 0;
-var down = 0;
-var up = 0;
+var frogMoveLeft = 0;
+var frogMoveRight = 0;
+var frogMoveDown = 0;
+var frogMoveUp = 0;
 var log = [];
 var car = [];
 var frog;
 var platform;
 var carArrayPositionX = []
 var logArrayPositionX = []
-var l = 0;
-var c = 0;
+var logXCounter = 0;
+var carXCounter = 0;
 
 for (var xCarPosition = 0; xCarPosition < 200; xCarPosition ++){
-  c += 350;
-  carArrayPositionX.push(c);
+  carXCounter += 350;
+  carArrayPositionX.push(carXCounter);
 }
 
 for(var xLogPosition = 0; xLogPosition <200; xLogPosition ++){
-  l -= 350;
-  logArrayPositionX.push(l);
+  logXCounter -= 350;
+  logArrayPositionX.push(logXCounter);
 }
 
 function preload() {
@@ -35,7 +35,6 @@ function preload() {
 function create() {
   frogScale = game.rnd.realInRange(0.1,0.1);
   frog = game.add.sprite(360, 600,'frog');
-
   game.physics.arcade.enable(frog);
   frog.body.collideWorldBounds = true;
   frog.scale.x = frogScale;
@@ -51,15 +50,12 @@ function create() {
     car[i].scale.y = carScale;
   }
 
-
-
   waterScale = game.rnd.realInRange(.5,.5);
   water = game.add.sprite(-170, 0,'water');
   water.scale.x = waterScale*2.2;
   water.scale.y = waterScale;
 
   for(var i = 0; i < 200; i ++){
-
   logScale = game.rnd.realInRange(0.2,0.2);
   log[i] = game.add.sprite(logArrayPositionX[i], 200,'log');
   game.physics.arcade.enable(log);
@@ -71,12 +67,10 @@ function create() {
 
 function update() {
 
-  left ++;
-  right ++;
-  down ++;
-  up ++;
-
-
+  frogMoveLeft ++;
+  frogMoveRight ++;
+  frogMoveDown ++;
+  frogMoveUp ++;
 
   cursors = game.input.keyboard.createCursorKeys();
   frog.body.velocity.x = 0;
@@ -99,25 +93,23 @@ function update() {
  }
 
 
-
   if (cursors.left.isDown){
-    if(left % 7 == 0){
+    if(frogMoveLeft % 7 == 0){
       frog.body.position.x -= 20;
-      console.log(left);
     }
   }
   if (cursors.right.isDown){
-    if(right % 7 == 0){
+    if(frogMoveRight % 7 == 0){
       frog.body.position.x += 20;
     }
   }
   if (cursors.up.isDown){
-    if(down % 7 == 0){
+    if(frogMoveDown % 7 == 0){
       frog.body.position.y -= 20;
     }
   }
   if (cursors.down.isDown){
-    if(up % 7 == 0){
+    if(frogMoveUp % 7 == 0){
       frog.body.position.y += 20;
     }
   }
