db3ea4a791a380eef93b80e28b7209509c6fe9de
Vlad Abacumov & Xang Thao <pair+vabacumov.xangthao24@devbootcamp.com>
2014-09-05 21:31:00 -0700

    Adds two tests to make sure frogger's staring position is within the horizontal bounaries

--- spec/spec/froggerSpec.js
+++ spec/spec/froggerSpec.js
@@ -4,4 +4,14 @@ describe("Frogger Game", function () {
       expect(carWidth).toEqual(50);
     });
   });
+  describe("frog", function() {
+    it("cannot leave left bound", function() {
+      expect(frog.x).toBeGreaterThan(frogRadius)
+    })
+    it("cannot leave right bound", function() {
+      expect(frog.x).toBeLessThan(borderWidth-frogRadius)
+    })
+    console.log(borderWidth)
+  })
 });
+
