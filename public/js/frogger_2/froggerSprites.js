a286a9a0b1f569036ab9f7525583692ab86e00e3
Hethe Berg <growthcode@gmail.com>
2014-09-06 01:24:25 -0700

    Got frogger SpriteSheet to select Frog and display animation. Binding still needed. Also added js reference on application html

--- public/js/frogger_2/froggerSprites.js
+++ public/js/frogger_2/froggerSprites.js
@@ -1,16 +1,38 @@
-var froggerSpriteData = {
-     images: ["froggerSprite.png"],
-     frames: {width:33, height:9},
-     animations: {
-                  frogFaceRight:[1,3],
-                  frogMoveRight:[4,7],
-                  frogFaceLeft:[1,3],
-                  frogMoveLeft:[4,8],
-                  frogLeft:[10,16]
-                  jump:[5,8,"run"]
-                }
- };
+var frogSpriteSheetTestMovements = new createjs.SpriteSheet({
+  animations: {
+                frogJumpRight:[0,1],
+                frogJumpLeft:[2,3],
+                frogJumpUp:[4,5],
+                frogJumpDown:[6,7]
+              },
+  images: ["../../assets/frogger_2/froggerSprite.png"],
+  frames: [
+            [13, 334, 17, 23],
+            [43, 335, 25, 22],
+            [82, 335, 18, 23],
+            [112, 338, 25, 22],
+            [12, 369, 23, 17],
+            [46, 366, 22, 25],
+            [80, 369, 23, 17],
+            [114, 366, 22, 25]
+          ]
+});
 
- var spriteSheet = new createjs.SpriteSheet(data);
- var animation = new createjs.Sprite(spriteSheet, "run");
+
+frogSpriteSheetTestMovements.getAnimation("frogJumpRight").speed = 5;
+frogSpriteSheetTestMovements.getAnimation("frogJumpRight").next = "frogJumpRight";
+frogSpriteSheetTestMovements.getAnimation("frogJumpRight").next = "frogJumpRight";
+frogSpriteSheetTestMovements.getAnimation("frogJumpRight").next = "frogJumpUp";
+frogSpriteSheetTestMovements.getAnimation("frogJumpUp").next = "frogJumpUp";
+frogSpriteSheetTestMovements.getAnimation("frogJumpUp").next = "frogJumpLeft";
+frogSpriteSheetTestMovements.getAnimation("frogJumpLeft").next = "frogJumpLeft";
+frogSpriteSheetTestMovements.getAnimation("frogJumpLeft").next = "frogJumpDown";
+frogSpriteSheetTestMovements.getAnimation("frogJumpDown").next = "frogJumpDown";
+frogSpriteSheetTestMovements.getAnimation("frogJumpDown").next = "frogJumpUp";
+
+var frogSprite = new createjs.Sprite(frogSpriteSheetTestMovements, "frogJumpRight");
+frogSprite.scaleY = frogSprite.scaleX = 2;
+
+stage.addChild(frogSprite);
+
 
