46216e30ab0759746db94dfb368c7236bed52a39
Antonio Dangond <antoniodangond@gmail.com>
2014-09-07 13:40:38 -0700

    Limits player movement within bounds of canvas.

--- public/js/dodgeIan/player.js
+++ public/js/dodgeIan/player.js
@@ -5,17 +5,29 @@ var Player = function(name, posX, posY, radius, gravatar){
   this.name = name;
   this.shape.x = posX;
   this.shape.y = posY;
-  this.stepSize = 10;
+  this.stepSize = 60;
   this.hp = 100;
   var that = this;
-  this.step = function(direction) {
-    if (direction == "left") {
-      that.shape.x -= this.stepSize;
-    } else {
-      that.shape.x += this.stepSize;
+  this.stepLeft = function() {
+    if((this.shape.x - this.radius) > 0) {
+      this.shape.x -= this.stepSize
     }
   }
-}
+  this.stepRight = function() {
+    if((this.shape.x + this.radius) < canvas.width) {
+      this.shape.x += this.stepSize
+    }
+  }
+  this.stepUp = function() {
+    if((this.shape.y - this.radius) > 0) {
+      this.shape.y -= this.stepSize
+    }
+  }
+  this.stepDown = function() {
+    if((this.shape.y + this.radius) < canvas.height) {
+      this.shape.y += this.stepSize
+    }
+  }
 
 var antonio = new Player("Antonio", 400, 200, 30);
 var xang = new Player("Xang", 500, 200, 30);
@@ -25,20 +37,35 @@ stage.addChild(xang.shape);
 stage.update();
 
 var movePlayer = function(event) {
-  if (event['keyCode'] === 39 ) {
-    antonio.step("right");
-    xang.step("right");
-    console.log("right");
+  if (event['keyCode'] === 37) {
+    xang.stepLeft()
     stage.update();
+    if(xang.shape.x <= xang.radius){
+      xang.shape.x = xang.radius;
+    }
   }
-  if (event['keyCode'] === 37 ) {
-    antonio.step("left");
-    xang.step("left");
-    console.log("left");
+  if (event['keyCode'] === 39) {
+    xang.stepRight()
     stage.update();
+    if(xang.shape.x >= canvas.width - xang.radius){
+      xang.shape.x = canvas.width - xang.radius;
+    }
+  }
+  if (event['keyCode'] === 38) {
+    xang.stepUp()
+    stage.update();
+    if(xang.shape.y <= xang.radius){
+      xang.shape.y = xang.radius;
+    }
+  }
+  if (event['keyCode'] === 40) {
+    xang.stepDown()
+    stage.update();
+    if(xang.shape.y >= canvas.height - xang.radius){
+      xang.shape.y = canvas.height - xang.radius;
+    }
   }
 }
 
-$(document).on('keyup', movePlayer);
-
+$(document).on('keydown', movePlayer);
 createjs.Ticker.addEventListener('tick', movePlayer);
