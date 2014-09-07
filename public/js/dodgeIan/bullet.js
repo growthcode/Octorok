2ee8d2ae85faf93f87c7d45c1f0a776875bbb4a1
Antonio Dangond is awesome!!! :) yt & Vlad Abacumov <pair+antoniodangond.vabacumov@devbootcamp.com>
2014-09-07 00:05:59 -0700

    Adds bullet movement logic. Bullet bounces off bottom and sides of canvas. NOTE: Need to move x,y,dx,and dy out of variable scope and rename.

--- public/js/dodgeIan/bullet.js
+++ public/js/dodgeIan/bullet.js
@@ -1,14 +1,36 @@
+WIDTH = canvas.width
+HEIGHT = canvas.height
+x = 20;
+y = 20;
+dx = 8;
+dy = 2;
+
 var Bullet = function (posX) {
   this.damage = 40;
   this.shape = new createjs.Shape();
-  this.shape.graphics.beginFill("green").drawCircle(0,0,2);
+  this.shape.graphics.beginFill("green").drawCircle(0,0,10);
   this.shape.x = posX;
-  this.shape.y = stage.canvas.height - 10;
+  this.shape.y = 100;
   this.velocity = 50;
   var that = this;
+  // this.move = function() {
+  //   that.shape.y -= that.velocity;
+  // }
   this.move = function() {
-    that.shape.y -= that.velocity;
+    that.shape.x = x
+    that.shape.y = y
+    stage.update()
+    if (x + dx > WIDTH || x + dx < 0) {
+        dx = -dx;
+      }
+    if (y + dy > HEIGHT) {
+      dy = -dy;
+     }
+    x += dx;
+    y += dy;
   }
+  stage.update();
+  console.log('move')
 }
 
 var sevenSixtyTwo = new Bullet(300);
@@ -21,3 +43,6 @@ var fireBullet = function() {
 
 stage.addChild(sevenSixtyTwo.shape);
 stage.update();
+
+setInterval(sevenSixtyTwo.move, 30)
+// createjs.Ticker.addEventListener('tick', fireBullet);
