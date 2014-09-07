625be6bc7d8bb1e7bde9e4aa5a9dcc0917e9999a
Vlad Abacumov <vabacumov@gmail.com>
2014-09-07 11:02:42 -0700

    Adjusts the fireBullet function in Ian model Minor indentation and colon fixes

--- public/js/dodgeIan/collisionDetector.js
+++ public/js/dodgeIan/collisionDetector.js
@@ -1,21 +1,17 @@
-var CollisionDetector = function(bullet, player){
-  this.bullet = bullet
-  this.player = player
-  var that = this
-  var _findDistance = function(){
-    that.distance = Math.sqrt(
-            ((bullet.shape.x - player.shape.x) * (bullet.shape.x - player.shape.x))
-          + ((bullet.shape.y - player.shape.y) * (bullet.shape.y - player.shape.y))
-           );
-   return that.distance
+var CollisionDetector = function(bullet, player) {
+  this.bullet = bullet;
+  this.player = player;
+  var that = this;
+  var _findDistance = function() {
+    var distance = Math.sqrt(
+    ((bullet.shape.x - player.shape.x) * (bullet.shape.x - player.shape.x))
+    + ((bullet.shape.y - player.shape.y) * (bullet.shape.y - player.shape.y))
+    );
+   return distance;
   }
-  this.bulletPlayerCollision = function () {
-    var distance =  _findDistance()
-    if (distance<(bullet.radius+player.radius)){
-    return true
+  this.bulletPlayerCollision = function() {
+    if (_findDistance() < (bullet.radius + player.radius)) {
+      return true;
     }
   }
 }
-
-var detector = new CollisionDetector(sevenSixtyTwo,antonio)
-createjs.Ticker.addEventListener('tick', detector.bulletPlayerCollision)
