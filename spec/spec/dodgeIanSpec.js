6474ed899d6ed14b4624a0eef072e9cbc37572c3
Vlad Abacumov <vabacumov@gmail.com>
2014-09-07 15:50:48 -0700

    Adds a new test for Ian model that implements spyOn feature of Jasmine Could be used a reference point for future tets!

--- spec/spec/dodgeIanSpec.js
+++ spec/spec/dodgeIanSpec.js
@@ -9,5 +9,11 @@ describe("Dodge Ian Game", function() {
       expect(bossIan.shape.x).toEqual(300);
       expect(bossIan.shape.y).toEqual(525);
     });
+    it("is redrawn after each move", function() {
+      var ian = new Ian();
+      spyOn(stage, 'update');
+      ian.move();
+      expect(stage.update).toHaveBeenCalled();
+    });
   });
 });
