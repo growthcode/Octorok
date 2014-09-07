29801e99d9ca9ae061aac86bc12b074ff82882fa
Vlad Abacumov <vabacumov@gmail.com>
2014-09-07 12:42:03 -0700

    Creates destroyBullet fn to remove the bullet from stage after it hits the player (Note that bullet object is not actually destroyed!!!)

--- public/js/dodgeIan/bullet.js
+++ public/js/dodgeIan/bullet.js
@@ -2,7 +2,7 @@ var Bullet = function (posX, posY, dx, dy) {
   this.radius = Math.random() * 10;
   this.damage = this.radius * 10;
   this.shape = new createjs.Shape();
-  this.shape.graphics.beginFill("green").drawCircle(0,0,radius);
+  this.shape.graphics.beginFill("green").drawCircle(0, 0, this.radius);
   this.shape.x = posX;
   this.shape.y = posY;
   this.dx = dx;
@@ -24,4 +24,11 @@ var fireBullet = function(bullet) {
   bullet.move();
   stage.update();
   console.log('fire');
+}
+
+var destroyBullet = function(bullet) {
+  var bulletIndex = stage.children.indexOf(bullet.shape);
+  stage.removeChildAt(bulletIndex);
+  delete bullet;
+  console.log(bullet + "was removed from canvas");
 }
