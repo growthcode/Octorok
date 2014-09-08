1741c9bd2704efcec4566456e6c2b60723860af5
Vlad Abacumov <vabacumov@gmail.com>
2014-09-07 17:12:13 -0700

    Adds tests for bulletPlayerCollision fn to verify that it returns true when a collision occurs

--- spec/spec/dodgeIanSpec.js
+++ spec/spec/dodgeIanSpec.js
@@ -23,5 +23,14 @@ describe("Dodge Ian Game", function() {
       var object2 = new Bullet(100, 200);
       expect(new CollisionDetector(object1, object2)._findDistance()).toEqual(100);
     });
+    
+    describe("bulletPlayerCollision", function() {
+      it("returns true if the collision occurs", function() {
+        var object1 = new Player("player", 100, 100, 10);
+        var object2 = new Bullet(100, 99);
+        var controller = new CollisionDetector(object1, object2);
+        expect(controller.bulletPlayerCollision()).toEqual(true);
+      });
+    });
   });
 });
