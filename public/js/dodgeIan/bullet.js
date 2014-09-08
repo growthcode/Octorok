2940c4f947e3539bf609191284f5c7f63f28b176
Hethe Berg <growthcode@gmail.com>
2014-09-08 12:02:37 -0700

    Bullet constants identified

--- public/js/dodgeIan/bullet.js
+++ public/js/dodgeIan/bullet.js
@@ -1,23 +1,32 @@
-var BULLETMINSIZE=6;
-var Bullet = function (posX, posY, dx, dy) {
-  this.radius = Math.random() * 10+BULLETMINSIZE;
-  this.damage = this.radius * 2;
+var BULLET_MIN_SIZE = 6,
+    BULLET_RADIUS = Math.random() * 10 + BULLET_MIN_SIZE,
+    DAMAGE_MULTIPLIER = 2,
+    BULLET_COLOR = "green",
+    BULLET_RELATIVE_ORIGIN_X = 0, // should not change
+    BULLET_RELATIVE_ORIGIN_Y = 0; // should not change
+
+
+
+
+var Bullet = function (positionX, positionY, velocityX, velocityY) {
+  this.radius = BULLET_RADIUS;
+  this.damage = this.radius * DAMAGE_MULTIPLIER;
   this.shape = new createjs.Shape();
-  this.shape.graphics.beginFill("green").drawCircle(0, 0, this.radius);
-  this.shape.x = posX;
-  this.shape.y = posY;
-  this.dx = dx;
-  this.dy = dy;
+  this.shape.graphics.beginFill(BULLET_COLOR).drawCircle(BULLET_RELATIVE_ORIGIN_X, BULLET_RELATIVE_ORIGIN_Y, this.radius);
+  this.shape.x = positionX;
+  this.shape.y = positionY;
+  this.velocityX = velocityX;
+  this.velocityY = velocityY;
   this.alive = true;
   var that = this;
   this.move = function() {
-    if (that.shape.x + dx > canvas.width || that.shape.x + dx < 0) {
-        dx = -dx;
+    if (that.shape.x + velocityX > canvas.width || that.shape.x + velocityX < 0) {
+        velocityX = -velocityX;
       }
-    if (that.shape.y + dy > canvas.height) {
-      dy = -dy;
+    if (that.shape.y + velocityY > canvas.height) {
+      velocityY = -velocityY;
      }
-    that.shape.x += dx;
-    that.shape.y += dy;
+    that.shape.x += velocityX;
+    that.shape.y += velocityY;
   }
 }
