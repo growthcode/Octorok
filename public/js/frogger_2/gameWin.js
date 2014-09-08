d81e2ff8143134cc0fce08a04069577bc8e80ede
Antonio Dangond <antoniodangond@gmail.com>
2014-09-07 19:49:24 -0700

    Creates controllers and event binding with for loop.

--- public/js/frogger_2/gameWin.js
+++ public/js/frogger_2/gameWin.js
@@ -13,6 +13,7 @@ Win.Controller = function(frog, slot) {
   this.checkSlot = function() {
     if(this.frog.x > this.slot.leftBound && this.frog.x < this.slot.rightBound && this.frog.y <= rowHeight/2) {
       this.slot.active = true
+      // removeSlot(slots, this.slot.id)
       console.log('Inside Slot '+this.slot.id+', '+slot.active)
     }
   }.bind(this)
@@ -24,28 +25,21 @@ slot3 = new Win.Slot(3, (columnWidth*7), (columnWidth*8));
 slot4 = new Win.Slot(4, (columnWidth*10), (columnWidth*11));
 slot5 = new Win.Slot(5, (columnWidth*13), (columnWidth*14));
 
-winController1 = new Win.Controller(frog, slot1);
-winController2 = new Win.Controller(frog, slot2);
-winController3 = new Win.Controller(frog, slot3);
-winController4 = new Win.Controller(frog, slot4);
-winController5 = new Win.Controller(frog, slot5);
-
-createjs.Ticker.addEventListener('tick', winController1.checkSlot);
-createjs.Ticker.addEventListener('tick', winController2.checkSlot);
-createjs.Ticker.addEventListener('tick', winController3.checkSlot);
-createjs.Ticker.addEventListener('tick', winController4.checkSlot);
-createjs.Ticker.addEventListener('tick', winController5.checkSlot);
-
 var slots = [slot1, slot2, slot3, slot4, slot5];
+for (var i in slots) {
+  var winController = new Win.Controller(frog, slots[i])
+  createjs.Ticker.addEventListener('tick', winController.checkSlot)
+}
 
-var findSlotById = function(array, id) {
+var findSlotById = function(array, slotId) {
   slot = $.grep(array, function(slot){
-    return slot.id === id;
+    return slot.id === slotId;
   })
   return slot
 };
 
-var removeSlot = function(array, slot) {
-  var index = array.indexOf(slot)
+var removeSlot = function(array, slotId) {
+  // slot = findSlotById(array, slotId)
+  var index = array.indexOf(slotId)
   array.splice(index, 1)
 }
