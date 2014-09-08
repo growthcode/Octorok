486f1ee4cd86127a9e24b8abcd9e4559638b4dc9
Xang Thao <xangthao24@yahoo.com>
2014-09-07 19:49:07 -0700

    Add 3rd lane rectangle

--- public/js/frogger_2/carsGround.js
+++ public/js/frogger_2/carsGround.js
@@ -3,10 +3,13 @@ var carHeight=rowHeight - (canvas.height/60);
 var truckWidth=180;
 var truckHeight=rowHeight - (canvas.height/60);
 
+xDifference = 300;
 car1StartXPos=100;
 car1StartYPos=borderHeight - rowHeight * 2 + (canvas.height/120);
 car2StartXPos=100;
 car2StartYPos=borderHeight - rowHeight * 3 + (canvas.height/120);
+carLane3StartXPos= 0;
+carLane3StartYPos=borderHeight - rowHeight * 4 + (canvas.height/120);
 truck1StartXPos=400;
 truck1StartYPos=borderHeight - rowHeight * 2 + (canvas.height/120);
 truck2StartXPos=400;
@@ -19,8 +22,10 @@ var logLane2Vel=10;
 
 var lane1 = [];
 var lane2 = [];
+var lane3 = [];
 var imgLane1 = [];
 var imgLane2 = [];
+
 
 
 
@@ -61,6 +66,8 @@ imagebus2.y = 440;
 imagebus2.scaleX = 0.3;
 imagebus2.scaleY = 0.3;
 
+
+
 var imgLane1 = [imageCar1, imageTruck1, imageBus1];
 var imgLane2 = [imageCar2, imagetruck2, imagebus2];
 
@@ -84,11 +91,32 @@ truck2.graphics.beginFill("white").drawRect(0, 0, truckWidth, truckHeight);
 truck2.x = truck2StartXPos;
 truck2.y = truck2StartYPos;
 
+var car4 = new createjs.Shape();
+car4.graphics.beginFill("red").drawRect(0, 0, carWidth, carHeight);
+car4.x = carLane3StartXPos + (xDifference *1);
+car4.y = carLane3StartYPos;
+
+var car5 = new createjs.Shape();
+car5.graphics.beginFill("blue").drawRect(0, 0, carWidth, carHeight);
+car5.x = carLane3StartXPos + (xDifference * 2);
+car5.y = carLane3StartYPos;
+
+var car6 = new createjs.Shape();
+car6.graphics.beginFill("green").drawRect(0, 0, carWidth, carHeight);
+car6.x = carLane3StartXPos + (xDifference * 3);
+car6.y = carLane3StartYPos;
+
+var car7 = new createjs.Shape();
+car7.graphics.beginFill("black").drawRect(0, 0, carWidth, carHeight);
+car7.x = carLane3StartXPos + (xDifference * 4 );
+car7.y = carLane3StartYPos;
+
+lane3 = [car4, car5, car6, car7];
 lane1.push(car1, truck1);
 lane2.push(car2, truck2);
 
 
-stage.addChild(car1, car2, truck1, truck2, imgLane2[0], imgLane2[1], imgLane1[0],imgLane1[1]);
+stage.addChild(car1, car2, truck1, truck2, imgLane2[0], imgLane2[1], imgLane1[0],imgLane1[1], car4, car5, car6, car7);
 stage.update();
 
 function tick(event) {
@@ -111,6 +139,12 @@ function tick(event) {
     if (lane2[i].x < 0 - 200) { lane2[i].x = 800 }
     lane2[i].x -= lane2Vel
   }
+
+  for (var i in lane3){
+    if (lane3[i].x > stage.canvas.width + 450){lane3[i].x = -200}
+      lane3[i].x += lane1Vel
+  }
+
   for (var i in logArray1) {
     if (logArray1[i].x > stage.canvas.width + 100) { logArray1[i].x = 0 }
     logArray1[i].x += logLane1Vel
