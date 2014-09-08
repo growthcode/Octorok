962d408c0c30f52f2050f1b685b51244986b7fa6
Antonio Dangond <antoniodangond@gmail.com>
2014-09-07 20:08:46 -0700

    Creates slots with for loop.

--- public/js/frogger_2/gameWin.js
+++ public/js/frogger_2/gameWin.js
@@ -1,7 +1,6 @@
 Win = {}
 
-Win.Slot = function(id, leftBound, rightBound) {
-  this.id = id
+Win.Slot = function(leftBound, rightBound) {
   this.leftBound = leftBound;
   this.rightBound = rightBound;
   this.active = false
@@ -19,13 +18,12 @@ Win.Controller = function(frog, slot) {
   }.bind(this)
 }
 
-slot1 = new Win.Slot(1, columnWidth, (columnWidth*2));
-slot2 = new Win.Slot(2, (columnWidth*4), (columnWidth*5));
-slot3 = new Win.Slot(3, (columnWidth*7), (columnWidth*8));
-slot4 = new Win.Slot(4, (columnWidth*10), (columnWidth*11));
-slot5 = new Win.Slot(5, (columnWidth*13), (columnWidth*14));
+var slots = []
+for(var i = 1; i < 14; i += 3) {
+  var slot = new Win.Slot((columnWidth*i), (columnWidth*(i+1)))
+  slots.push(slot)
+}
 
-var slots = [slot1, slot2, slot3, slot4, slot5];
 for (var i in slots) {
   var winController = new Win.Controller(frog, slots[i])
   createjs.Ticker.addEventListener('tick', winController.checkSlot)
