3c8749e91c9facc0b6ac95ed4aa5cfd61efde4db
Antonio Dangond <antoniodangond@gmail.com>
2014-09-07 20:28:08 -0700

    Creates Win.Factory, which creates slots and controllers.

--- public/js/frogger_2/gameWin.js
+++ public/js/frogger_2/gameWin.js
@@ -1,5 +1,21 @@
 Win = {}
 
+Win.Factory = function() {
+  this.slots = [];
+  this.createSlots = function() {
+    for(var i = 1; i < 14; i += 3) {
+      var slot = new Win.Slot((columnWidth*i), (columnWidth*(i+1)))
+      this.slots.push(slot)
+    }
+  };
+  this.createControllers = function() {
+    for (var i in this.slots) {
+      var controller = new Win.Controller(frog, this.slots[i])
+      createjs.Ticker.addEventListener('tick', controller.checkSlot)
+    }
+  }.bind(this)
+}
+
 Win.Slot = function(leftBound, rightBound) {
   this.leftBound = leftBound;
   this.rightBound = rightBound;
@@ -13,21 +29,15 @@ Win.Controller = function(frog, slot) {
     if(this.frog.x > this.slot.leftBound && this.frog.x < this.slot.rightBound && this.frog.y <= rowHeight/2) {
       this.slot.active = true
       // removeSlot(slots, this.slot.id)
-      console.log('Inside Slot '+this.slot.id+', '+slot.active)
+      console.log('Inside Slot '+slot.active)
     }
   }.bind(this)
 }
 
-var slots = []
-for(var i = 1; i < 14; i += 3) {
-  var slot = new Win.Slot((columnWidth*i), (columnWidth*(i+1)))
-  slots.push(slot)
-}
+factory = new Win.Factory();
+factory.createSlots();
+factory.createControllers();
 
-for (var i in slots) {
-  var winController = new Win.Controller(frog, slots[i])
-  createjs.Ticker.addEventListener('tick', winController.checkSlot)
-}
 
 var findSlotById = function(array, slotId) {
   slot = $.grep(array, function(slot){
