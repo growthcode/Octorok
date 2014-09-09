df009bc6f4ab7f0df05630fc558df54ac639c124
Vlad Abacumov <vabacumov@gmail.com>
2014-09-08 20:56:32 -0700

    Adds finish line sprite with 5 slots to the canvas Restructures canvas width based on finish line sprite's width (16 columns) Restructures canvas height based on log sprite's height (13 rows + height of finish line) Removes junk code and unnecessary files

--- public/js/frogger_2/application.js
+++ public/js/frogger_2/application.js
@@ -1,22 +1,25 @@
+var landFinish = new createjs.Sprite(froggerSpriteData, "winningGrass");
+landFinish.x = 0;
+landFinish.y = 0;
+
 var canvas = document.createElement('canvas');
     canvas.id = "game-canvas"
-    canvas.width = 690;
-    canvas.height = 598;
+    canvas.width = landFinish.getBounds().width;
+    canvas.height = 327;
     canvas.style.border = "1px solid black";
     document.body.appendChild(canvas);
 var stage = new createjs.Stage('game-canvas');
 
-var borderWidth=stage.canvas.width;
-var borderHeight=stage.canvas.height;
-var rowHeight = stage.canvas.height/13
-var columnWidth = stage.canvas.width/15
 
-var Log = function(posX, posY, direction) {
-  this.direction = direction;
-  this.x = posX;
-  this.y = posY;
-  this.height = rowHeight;
-}
+stage.addChild(landFinish);
+stage.update();
+
+var borderWidth = stage.canvas.width;
+var borderHeight = stage.canvas.height;
+var finishLineBoundary = landFinish.getBounds().height;
+var rowHeight = (stage.canvas.height - finishLineBoundary) / 13;
+var columnWidth = stage.canvas.width / 16;
+
 
 Log.prototype = new createjs.Sprite(froggerSpriteData, "medLog");
 Log.prototype.speed = 10;
