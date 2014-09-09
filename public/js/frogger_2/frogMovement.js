bc0ff26b4d155ffddcf07b529b074db7d634e8d2
Hethe Berg <growthcode@gmail.com>
2014-09-09 02:31:23 -0700

    rmoved repeate line

--- public/js/frogger_2/frogMovement.js
+++ public/js/frogger_2/frogMovement.js
@@ -3,8 +3,7 @@ var moveDistance = borderHeight / 13
 var frogXStart=borderWidth/2;
 var frogYStart=borderHeight - frogRadius;
 
-var frog = new createjs.Sprite(froggerSpriteData, "frogJumpUp");
-var frog = new createjs.Sprite(froggerSpriteData, "frogJumpUp");
+var frog = new createjs.Sprite(froggerSpriteData);
 stage.addChild(frog);
 stage.update();
 
@@ -17,27 +16,27 @@ function movefrog(event){
     if(frog.x >= (borderWidth-frogRadius)){
       frog.x = borderWidth-frogRadius;
     }
-    frog.gotoAndPlay("frogJumpRight").stop();
+    frog.gotoAndPlay("frogJumpRight");
   }
   if (event['keyCode'] === 37 ) {
     frog.x -= moveDistance
     if(frog.x <= frogRadius){
       frog.x = frogRadius;
     }
-    frog.gotoAndPlay("frogJumpLeft").stop();
+    frog.gotoAndPlay("frogJumpLeft");
   }
   if (event['keyCode'] === 40 ) {
     frog.y += moveDistance
     if(frog.y >= (borderHeight - frogRadius)){
       frog.y = borderHeight - frogRadius;
     }
-    frog.gotoAndPlay("frogJumpDown").stop();
+    frog.gotoAndPlay("frogJumpDown");
   }
   if (event['keyCode'] === 38 ) {
     frog.y -= moveDistance
     if(frog.y <= frogRadius){
       frog.y = frogRadius;
     }
-    frog.gotoAndPlay("frogJumpUp").stop();
+    frog.gotoAndPlay("frogJumpUp");
   }
 }
