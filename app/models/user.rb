51f4483d3be186cb1765240f45818542f53c8b90
Frank S <fhshaw7@gmail.com>
2014-09-09 09:19:08 -0700

    Adds bcrypt to env and gemlist

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   validates  :first_name, presence: true
   validates  :last_name, presence: true
-  # validates  :user_name, presence: true: uniqueness: true
+  validates  :username, presence: true, uniqueness: true
   validates  :email, presence: true, uniqueness: true
   validates  :password_hash, presence: true, length: { minimum: 6 }
+
+
+
+
 end
