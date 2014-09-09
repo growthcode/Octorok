a1c1cfc0f4ff49e915b8108e65ad204dba62be50
Antonio Dangond <antoniodangond@gmail.com>
2014-09-08 17:52:28 -0700

    Adds function for checking rectangle-to-rectangle collision.

--- public/js/frogger_2/collisionDetection.js
+++ public/js/frogger_2/collisionDetection.js
@@ -35,6 +35,19 @@ frog['radius'] = frogRadius
 // car15['width'] = carWidth
 // car15['height'] = carHeight
 
+// rectangle
+checkIntersection = function(rect1,rect2) {
+  if (rect1.x > rect2.x + rect2.width && rect1.x + rect1.width < rect2.x && rect1.y > rect2.y + rect2.height && rect1.y + rect1.height < rect2.y ) {
+    return false
+  };
+  return true;
+}
+
+
+
+
+
+
 var resetFrogPosition = function() {
   frog.x = frogXStart
   frog.y = frogYStart
