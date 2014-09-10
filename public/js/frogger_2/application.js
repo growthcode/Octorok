7568124feda3f0968c20ea01e3b4d1715cf3da63
Hethe Berg <growthcode@gmail.com>
2014-09-09 19:23:29 -0700

    added gameBottomStart and replaced canvas.height

--- public/js/frogger_2/application.js
+++ public/js/frogger_2/application.js
@@ -5,9 +5,9 @@ landFinish.y = 0;
 var canvas = document.createElement('canvas');
 canvas.id = "frogger-2-game-canvas"
 canvas.width = landFinish.getBounds().width;
-canvas.height = 455;
+canvas.height = 503;
 
-var gameBottomStart = 418; // canvas.height former size before game boundaries
+var gameBottomStart = 473; // canvas.height former size before game boundaries
 canvas.style.border = "1px solid black";
 document.body.appendChild(canvas);
 
@@ -22,9 +22,9 @@ var columnWidth = canvas.width / 16;
 var verticalMoveDistance = rowHeight;
 var horizontalMoveDistance = columnWidth;
 
-var gameBoundaryHeight = canvas.height - finishLineBoundary
+var gameBoundaryHeight = gameBottomStart - finishLineBoundary
 
 var frogWidth = 26;
 var frogHeight = 26;
 var frogXStart = (canvas.width / 2) - frogWidth / 2;
-var frogYStart = (canvas.height - gameBoundaryHeight / 13) - (rowHeight - 26)/2;
+var frogYStart = (gameBottomStart - gameBoundaryHeight / 13) - (rowHeight - 26)/2;
