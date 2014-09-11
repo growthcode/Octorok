ee74f4bfff28841d222b7443f4293c0c395ef9a5
Antonio Dangond <antoniodangond@gmail.com>
2014-09-10 19:38:28 -0700

    Adjusts font and positioning of username on game screen.

--- public/js/frogger_2/controller.js
+++ public/js/frogger_2/controller.js
@@ -312,8 +312,9 @@ $.ajax({
   url: "/users/"+"user.id",
   type: 'get'
 }).done(function(data) {
-  var userText = new createjs.Text(data.username, "bold 22px Arial", "#00c200")
-  userText.x = canvas.width / 2 - 20
-  userText.y = canvas.height - 30
+  var userText = new createjs.Text(data.username +": ", "bold 22px Courier New", "#007600")
+  // userText.x = canvas.width / 2 - 20
+  userText.x = gameController.frogLivesContainer.x + gameController.frogLivesContainer.getBounds().width + 10
+  userText.y = canvas.height - 28
   stage.addChild(userText)
 })
