9953624b1747a00ac50919b3a62d6a9c21a862b0
Hethe Berg <growthcode@gmail.com>
2014-09-10 18:46:55 -0700

    added new background, add 2 pics for background, took out old background

--- public/js/frogger_2/controller.js
+++ public/js/frogger_2/controller.js
@@ -257,7 +257,7 @@ Game.Controller.prototype.checkAllSlots = function() {
 
 Game.Controller.prototype.gameSceneSetup = function(){
   this.frogLivesContainer.x = 5;
-  this.frogLivesContainer.y = gameBottomStart;
+  this.frogLivesContainer.y = gameBottomStart + 5;
   this.addLives(3);
   stage.addChild(this.frogLivesContainer);
   this.logCreator();
